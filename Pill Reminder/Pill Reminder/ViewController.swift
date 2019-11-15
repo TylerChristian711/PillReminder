896f6970a98cac7f945ab048ded8838885c736e4
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-15 16:50:57 -0500

    Added Xcode Project

---
+++ Pill Reminder/Pill Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pill Reminder
+//
+//  Created by Chad Rutherford on 11/15/19.
+//  Copyright © 2019 Chad & Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
