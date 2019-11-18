956238e0e7f7f10d61d8bc73932c9b931294f5b1
Tyler Christian <tylerc71197@gmail.com>
2019-11-18 16:51:38 -0500

    UI layouts and files for said layouts

---
+++ Pill Reminder/Pill Reminder/Controller/PillsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PillsTableViewCell.swift
+//  Pill Reminder
+//
+//  Created by Lambda_School_Loaner_218 on 11/18/19.
+//  Copyright © 2019 Chad & Tyler. All rights reserved.
+//
+
+import UIKit
+
+class PillsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
